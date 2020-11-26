34448ccf3a22675dd77dcdf948ae642166c6ab55
u1261969 <u1261969@umail.utah.edu>
2020-11-25 20:24:19 -0700

    Added twitch image maker

---
+++ textImageGenerators/twitchText.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+inputFile = open(sys.argv[1],'r')
+lines = inputFile.readlines()
+
+outpath = 'efs/'
+tempTextFilePath = outpath + 'twitchInstance.txt'
+tempTextFile = open(tempTextFilePath, 'w')
+
+for line in lines:
+    outFileName = line.strip() + ".png"
+    outFileName = outFileName.replace("/", "-")
+    outFileName = outFileName.replace(" ", "-")
+
+    ffmpegLine = "yes | ffmpeg -i blankTwitch.png -vf drawtext=\"fontsize=28:fontcolor=white:textfile='" + tempTextFilePath \
+    + ":x=(w-text_w)/2:y=(h-text_h + 20)/2:fontfile='Modenine-2OPd.ttf':font='ModeNine'\" -pix_fmt rgba " + outpath + outFileName
+
+    tempTextFile = open(tempTextFilePath, 'w')
+    tempTextFile.truncate()
+    tempTextFile.write("   Number of Viewers: " + line.split(' ')[0] + "\n")
+    tempTextFile.close()
+    os.system(ffmpegLine)
+    
+
+
+
